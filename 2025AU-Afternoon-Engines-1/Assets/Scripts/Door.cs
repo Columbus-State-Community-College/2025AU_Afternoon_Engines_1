93fcd06ada07c176b0aa9f4a29d4c248ab2f09dc
kerhard-153 <kerhard@student.cscc.edu>
2025-12-09 18:14:24 -0500

    Fixed doors and added more to the scene

--- 2025AU-Afternoon-Engines-1/Assets/Scripts/Door.cs
+++ 2025AU-Afternoon-Engines-1/Assets/Scripts/Door.cs
@@ -1,21 +1,47 @@
 using UnityEngine;
+using System.Collections;
 
 public class Door : MonoBehaviour
 {
-    public Animator animator;
-    private bool isOpen;
-
     AudioManager audioManager;
+
+    public bool isOpen = false;
+    public float openAngle = 90f;
+    public float openSpeed = 2f;
+
+    private Quaternion closedRot;
+    private Quaternion openRot;
 
 
     private void Awake()
     {
         audioManager = GameObject.FindGameObjectWithTag("AudioManager").GetComponent<AudioManager>();
     }
+
+    private void Start()
+    {
+        closedRot = transform.localRotation;
+        openRot = Quaternion.Euler(0, openAngle, 0) * closedRot;
+    }
     public void ToggleDoor()
     {
         isOpen = !isOpen;
-        animator.SetBool("isOpen", isOpen);
-       audioManager.PlaySFX(audioManager.doorOpen);
+        StopAllCoroutines();
+        StartCoroutine(AnimateDoor());
+        audioManager.PlaySFX(audioManager.doorOpen);
+    }
+
+    IEnumerator AnimateDoor()
+    {
+        Quaternion target = isOpen ? openRot : closedRot;
+        Quaternion start = transform.localRotation;
+
+        float t = 0;
+        while (t < 1f)
+        {
+            t += Time.deltaTime * openSpeed;
+            transform.localRotation = Quaternion.Slerp(start, target, t);
+            yield return null;
+        }
     }
 }
